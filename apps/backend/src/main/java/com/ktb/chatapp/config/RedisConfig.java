2a2f0c68f36bbe6f1af8ea3589ea1e848d41005c
hoooonshub <yhn515@naver.com>
2025-12-10 16:35:45 +0900

    feat: Redis 설정 파일 추가(Lettuce redis)

---
+++ apps/backend/src/main/java/com/ktb/chatapp/config/RedisConfig.java
@@ -0,0 +1,54 @@
+package com.ktb.chatapp.config;
+
+import io.lettuce.core.ReadFrom;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStaticMasterReplicaConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.master.host}")
+    private String masterHost;
+
+    @Value("${spring.data.redis.master.port}")
+    private Integer masterPort;
+
+    @Value("${spring.data.redis.replica.host}")
+    private String replicaHost;
+
+    @Value("${spring.data.redis.replica.port}")
+    private Integer replicaPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .readFrom(ReadFrom.REPLICA_PREFERRED)
+                .build();
+
+        RedisStaticMasterReplicaConfiguration redisConfig = new RedisStaticMasterReplicaConfiguration(masterHost, masterPort);
+        redisConfig.addNode(replicaHost, replicaPort);
+        return new LettuceConnectionFactory(
+                redisConfig,
+                clientConfig
+        );
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+
+}
